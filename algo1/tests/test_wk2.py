a1094235dec2cf7561f1cc3cededc0bbf2d1e487
Magnus Berg Sletfjerding <ms@corti.ai>
2021-02-22 13:58:25 +0100

    last commit before reimplementing

--- algo1/tests/test_wk2.py
+++ algo1/tests/test_wk2.py
@@ -6,6 +6,7 @@ from algo1.wk2assn import num_inversions
 @pytest.mark.parametrize(
     "x,n_true",
     [
+        ((1, 2, 4, 3), 1),
         ((1, 2, 3, 5, 4, 6), 1),
         ((1, 2, 5, 3, 4, 6), 2),
         ((1, 3, 5, 2, 4, 6), 3),
--- algo1/wk2assn.py
+++ algo1/wk2assn.py
@@ -1,4 +1,41 @@
+import logging
 import array
+
+LOGGER = logging.getLogger(__name__)
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)25s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+LOGGER.setLevel(logging.DEBUG)
+
+
+def merge_and_count_splits(arr1, arr2):
+    LOGGER.debug(f"arr1 : {arr1}")
+    LOGGER.debug(f"arr2 : {arr2}")
+    arr1_l = len(arr1)
+    arr2_l = len(arr2)
+    n = sum((arr1_l, arr2_l))
+    i, j, invs = 0, 0, 0
+    arr_sorted = array.array("I", [0] * n)
+
+    for k in range(n):
+        # end case 1: no more items in arr1, append all remaining items in arr2
+        if i >= arr1_l:
+            arr_sorted[k:] = arr2[j:]
+            break
+        # end case 2: no more items in arr2
+        if j >= arr2_l:
+            arr_sorted[k:] = arr1[i:]
+            invs += n - i
+            break
+
+        l, r = arr1[i], arr2[j]
+        if l < r:
+            arr_sorted[k] = l
+            i += 1
+        else:
+            arr_sorted[k] = r
+            j += 1
+            invs += 1
+    return arr_sorted, invs
 
 
 def count_split_inv(x: array.ArrayType) -> tuple:
@@ -6,13 +43,16 @@ def count_split_inv(x: array.ArrayType) -> tuple:
 
 
 def sort_and_count(x: array.ArrayType) -> tuple:
+    LOGGER.debug(f"x    : {type(x)} --> {x}")
     n = len(x)
-    if n == 1:
-        return 0
-    b, x = sort_and_count(x[: n // 2])
-    c, y = sort_and_count(x[n // 2 :])
-    d, z = count_split_inv(a)
-    return sum((x, y, z))
+    if n <= 1:
+        return x, 0
+    xl = x[: n // 2]
+    xr = x[n // 2 :]
+    b, x = sort_and_count(xl)
+    c, y = sort_and_count(xr)
+    d, z = merge_and_count_splits(b, c)
+    return d, sum((x, y, z))
 
 
 def num_inversions(x) -> int:
@@ -30,16 +70,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Number of inversions",
     )
-    parser.add_argument("spec", help="", default="(1, 2, 4, 3)")
+    parser.add_argument("--spec", help="", default="(1, 2, 4, 3)")
     args = parser.parse_args()
 
-    try:
+    if Path(args.spec).exists():
         p = Path(args.spec)
         assert p.exists()
         numbers = p.read_text().splitlines()
-    except AssertionError:
+    else:
         numbers = literal_eval(args.spec)
 
-    print(f"Found {len(numbers)} numbers")
+    LOGGER.info(f"Found {len(numbers)} numbers")
 
-    print(num_inversions(numbers))
+    LOGGER.info(sort_and_count(x=array.array("I", numbers)))
