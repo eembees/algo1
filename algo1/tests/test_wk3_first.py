922054482a18e0d5a1329a9465abf73e7f4845a5
Magnus Berg Sletfjerding <ms@corti.ai>
2021-02-23 17:45:17 +0100

    Made the first assn for week 3 work

---
+++ algo1/tests/test_wk3_first.py
@@ -0,0 +1,6 @@
+import pytest
+
+from algo1.wk3assn_first import quicksort_and_count
+
+
+# python ../../stanford-algs/tester/python3/tester.py ./wk3assn_first.py ../../stanford-algs/testCases name=quicksort_and_count
--- algo1/wk3assn_first.py
+++ algo1/wk3assn_first.py
@@ -5,8 +5,8 @@ from typing import Tuple, AnyStr
 from itertools import repeat
 import math
 
-log_level = os.getenv("LOG_LEVEL", "INFO")
-log_level = {"INFO": logging.INFO, "DEBUG": logging.DEBUG}.get(log_level)
+log_level = os.getenv("LOG_LEVEL", "CRITICAL") # TODO: make fix wrt this in some nice way
+log_level = {"INFO": logging.INFO, "DEBUG": logging.DEBUG, "CRITICAL":logging.CRITICAL}.get(log_level)
 
 LOGGER = logging.getLogger(__name__)
 FORMAT = "[%(filename)s:%(lineno)3s - %(funcName)25s() ] %(message)s"
@@ -15,13 +15,15 @@ LOGGER.setLevel(log_level)
 
 
 def swap(arr, i, j):
-    _temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = _temp
-    LOGGER.debug(f"{arr} \t swapped i={i}<-->j={j}: ")
+    if i != j:
+        _temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = _temp
+        LOGGER.debug(f"{arr} \t| swapped arr[{i}]={arr[j]} <--> arr[{j}]={arr[i]}: ")
 
 
 def partition(arr: array.ArrayType, l: int, r: int) -> Tuple[int, int]:
+    LOGGER.debug(f"{arr} \t| l={l}, r={r} \t\t| size of subarr: {str(r-l):2s}")
     p = arr[l]
     i = l + 1
     for j in range(l + 1, r + 1):
@@ -30,20 +32,24 @@ def partition(arr: array.ArrayType, l: int, r: int) -> Tuple[int, int]:
             swap(arr, i, j)
             i += 1
     # final pivot swap
+    LOGGER.debug(f"{arr} \t| Before placing pivot at pos {i - 1}")
     swap(arr, l, i - 1)
-
+    LOGGER.debug(f"{arr} \t| After  placing pivot at pos {i - 1}")
     return r - l, i - 1
 
 
 def quicksort_and_count(arr: array.ArrayType, start: int, stop: int) -> int:
-    LOGGER.debug(f"{arr} \t start: {str(start):2s} \t stop: {str(stop):2s} \t  subarr: {arr[start:stop+1]}")#\t arr id: {id(arr)}")
-    if stop-start <= 1:
+    LOGGER.debug(f"{arr} \t| Idxs: arr[{start:>2}:{stop+1: <2}] --> \t| {list(arr[start:stop+1])}")#\t arr id: {id(arr)}")
+    if stop-start < 1:
         return 0
+
     m_p, i_p = partition(arr, start, stop)
+
     # sort first part
-    LOGGER.debug(f"{arr} \t Partition done: pivot={arr[i_p]} i_p={i_p} m_p={m_p} ")
+    LOGGER.debug(f"{arr} \t| Partition done:\t| pivot={arr[i_p]} i_p={i_p} m_p={m_p} ")
+    # exit()
     # LOGGER.debug(f"Partition returned: m_p={m_p}, i_p={i_p}")
-    m_1 = quicksort_and_count(arr, start=start, stop=i_p)
+    m_1 = quicksort_and_count(arr, start=start, stop=i_p-1)
     m_2 = quicksort_and_count(arr, start=i_p + 1, stop=stop)
     # LOGGER.debug(f" m_p + m_1 + m_2 = {m_p}+{m_1}+{m_2}={m_p + m_1 + m_2}")
     return m_p + m_1 + m_2
@@ -57,7 +63,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Number of comparisons in quicksort",
     )
-    parser.add_argument("--spec", help="", default="(4, 5, 2, 3, 1)")
+    parser.add_argument("--spec", help="", default="(4, 5, 2, 3, 1)") # should give 7 
     # parser.add_argument("--spec", help="", default="(3, 2, 0, 4, 1, 7, 6, 5)")
     args = parser.parse_args()
 
@@ -71,5 +77,7 @@ if __name__ == "__main__":
     arr = array.array("I", map(int, numbers))
 
     LOGGER.debug(str(arr) + " <-- original")
-    LOGGER.info(quicksort_and_count(arr, 0, len(arr) - 1))
+    ans = quicksort_and_count(arr, start=0, stop=len(arr) - 1)
+    LOGGER.info(ans)
     LOGGER.debug(str(arr) + " <-- sorted")
+    print(ans)
