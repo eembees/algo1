b48cbe12cc18196f0f8f0cd8012ab93dced2f717
Magnus Berg Sletfjerding <ms@corti.ai>
2021-02-27 11:06:23 +0100

    Almost all test cases pass for almost all cases

---
+++ algo1/tests/test_wk3.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import logging
+import array
+from pathlib import Path
+import pytest
+from operator import itemgetter
+
+import context
+from algo1.wk3assn import quicksort_and_count_first, quicksort_and_count_last,quicksort_and_count_med
+
+
+MAX_LEN = int(os.getenv("MAX_TEST_LEN", "100"))
+
+log_level = {
+    "CRITICAL": logging.CRITICAL,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG,
+}.get(
+    os.getenv("LOG_LEVEL", "INFO")
+)  # TODO: make fix wrt this in some nice way)
+
+
+LOGGER = logging.getLogger(__name__)
+FORMAT = "[%(filename)s:%(lineno)3s - %(funcName)25s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+LOGGER.setLevel(log_level)
+
+
+def load_test_cases(test_cases_path: Path):
+    cases = []  # list of dicts
+    for test_case_path in test_cases_path.glob("input_*.txt"):
+        answer_path: Path = test_case_path.parent / test_case_path.name.replace(
+            "input", "output"
+        )
+        length = int(answer_path.stem.split("_")[-1])
+
+        answers = tuple(map(int, answer_path.read_text().splitlines()))
+
+        LOGGER.debug(
+            f"Found test case at: {str(test_case_path.name): <30s} with answers {str(answers) : <10s} and lenght {length: <10d}"
+        )
+
+        cases.append({"path": test_case_path, "answers": answers, "length": length})
+
+    return cases
+
+
+def test_wk3_single(quicksort_f, path, answer):
+    arr = array.array("I", map(int, path.read_text().splitlines()))
+    LOGGER.info(f"Case: {str(test_case_path.name): <30s} | LEN: {len(arr)}")
+    pred = quicksort_f(arr, start=0, stop=len(arr) - 1)
+
+    LOGGER.info(
+        f"Case: {str(test_case_path.name): <30s} | TRUE: {answer: >10d} | {pred: <10d} PRED | {'PASSED' if answer==pred else 'FAILED'}"
+    )
+    return answer == pred
+
+
+
+def test_wk3_first(cases):
+    results = [
+        test_wk3_single(quicksort_and_count_first,_p, _ans[0])
+        for _p, _ans in map(
+            itemgetter("path", "answers"),
+            filter(
+                lambda x: x.get("length") <= MAX_LEN,
+                sorted(cases, key=itemgetter("length")),
+            ),
+        )
+    ]
+    LOGGER.info(
+        f"PASS RATE: {sum(results): <5d}/{len(results): <5d} = {float(sum(results))/len(results):.0%}"
+    )
+
+
+def test_wk3_last(cases):
+    results = [
+        test_wk3_single(quicksort_and_count_last, _p, _ans[1])
+        for _p, _ans in map(
+            itemgetter("path", "answers"),
+            filter(
+                lambda x: x.get("length") <= MAX_LEN,
+                sorted(cases, key=itemgetter("length")),
+            ),
+        )
+    ]
+    LOGGER.info(
+        f"PASS RATE: {sum(results): <5d}/{len(results): <5d} = {float(sum(results))/len(results):.0%}"
+    )
+
+def test_wk3_med(cases):
+    results = [
+        test_wk3_single(quicksort_and_count_med, _p, _ans[2])
+        for _p, _ans in map(
+            itemgetter("path", "answers"),
+            filter(
+                lambda x: x.get("length") <= MAX_LEN,
+                sorted(cases, key=itemgetter("length")),
+            ),
+        )
+    ]
+    LOGGER.info(
+        f"PASS RATE: {sum(results): <5d}/{len(results): <5d} = {float(sum(results))/len(results):.0%}"
+    )
+
+
+if __name__ == "__main__":
+    test_case_path = (
+        Path(__file__).absolute().parent.parent.parent.parent
+        / "stanford-algs/testCases/course1/assignment3Quicksort"
+    )
+    cases = load_test_cases(test_case_path)
+    LOGGER.info(f"Found {len(cases):>5d} test cases")
+
+    test_wk3_first(cases)
+    test_wk3_last(cases)
+    test_wk3_med(cases)
---
+++ algo1/wk3assn.py
@@ -0,0 +1,163 @@
+import os
+import logging
+import array
+from typing import Tuple, AnyStr
+from itertools import repeat
+import math
+
+log_level = os.getenv("LOG_LEVEL", "INFO")  # TODO: make fix wrt this in some nice way
+log_level = {
+    "CRITICAL": logging.CRITICAL,
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG,
+}.get(log_level)
+
+LOGGER = logging.getLogger(__name__)
+FORMAT = "[%(filename)s:%(lineno)3s - %(funcName)25s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+LOGGER.setLevel(log_level)
+
+
+def swap(arr, i, j):
+    if i != j:
+        _temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = _temp
+        if LOGGER.isEnabledFor(logging.DEBUG):
+            LOGGER.debug(
+                f"{arr} \t| swapped arr[{i}]={arr[j]} <--> arr[{j}]={arr[i]}: "
+            )
+
+
+def partition(arr: array.ArrayType, l: int, r: int) -> Tuple[int, int]:
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f"{arr} \t| l={l}, r={r} \t\t| size of subarr: {str(r-l):2s}")
+    p = arr[l]
+    i = l + 1
+    for j in range(l + 1, r + 1):
+        if arr[j] < p:
+            # swap i and j index
+            swap(arr, i, j)
+            i += 1
+    # final pivot swap
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f"{arr} \t| Placing pivot {p: >3d} at pos {i - 1} ({p-1})")
+    swap(arr, l, i - 1)
+    return r - l, i - 1
+
+
+def quicksort_and_count_first(arr: array.ArrayType, start: int, stop: int) -> int:
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Quicksort: arr[{start:>2}:{stop+1: <2}] --> \t| {list(arr[start:stop+1])}"
+        )
+    if stop - start < 1:
+        return 0
+
+    m_p, i_p = partition(arr, start, stop)
+
+    # sort first part
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Partition done:\t| pivot={arr[i_p]} i_p={i_p} m_p={m_p} "
+        )
+    m_1 = quicksort_and_count_first(arr, start=start, stop=i_p - 1)
+    m_2 = quicksort_and_count_first(arr, start=i_p + 1, stop=stop)
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f" m_p + m_1 + m_2 = {m_p}+{m_1}+{m_2}={m_p + m_1 + m_2}")
+    return m_p + m_1 + m_2
+
+
+def quicksort_and_count_last(arr: array.ArrayType, start: int, stop: int) -> int:
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Quicksort: arr[{start:>2}:{stop+1: <2}] --> \t| {list(arr[start:stop+1])}"
+        )
+    if stop + 1 - start <= 1:
+        return stop + 1 - start
+
+    swap(arr, start, stop)
+    m_p, i_p = partition(arr, start, stop)
+
+    # sort first part
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Partition done:\t| pivot={arr[i_p]} i_p={i_p} m_p={m_p} "
+        )
+    m_1 = quicksort_and_count_last(arr, start=start, stop=i_p - 1)
+    m_2 = quicksort_and_count_last(arr, start=i_p + 1, stop=stop)
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f" m_p + m_1 + m_2 = {m_p}+{m_1}+{m_2}={m_p + m_1 + m_2}")
+    return m_p + m_1 + m_2
+
+
+def get_piv_idx_med(arr, start, stop):
+    idx_mid = start + round((stop - start) / 2 )
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Pivot cands: 1: arr[{start}, {idx_mid}, {stop}] = {arr[start]}, {arr[idx_mid]}, {arr[stop]} "
+        )
+
+    if idx_mid in (start, stop):
+        return idx_mid
+    _t = arr[start], arr[stop], arr[idx_mid]
+    for idx in start, stop, idx_mid:
+        if arr[idx] < max(_t) and arr[idx] > min(_t):
+            return idx
+
+
+def quicksort_and_count_med(arr: array.ArrayType, start: int, stop: int) -> int:
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Quicksort: arr[{start:>2}:{stop+1: <2}] --> \t| {list(arr[start:stop+1])}"
+        )
+    if stop - start < 1:
+        return 0
+
+    # set pivot (1st el) as median of right left and middle element
+    _idx_piv = get_piv_idx_med(arr, start, stop)
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f"{arr} \t| Pivot el arr[{_idx_piv}]={arr[_idx_piv]}")
+    swap(arr, start, _idx_piv)
+
+    m_p, i_p = partition(arr, start, stop)
+
+    # sort first part
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(
+            f"{arr} \t| Partition done:\t| pivot={arr[i_p]} i_p={i_p} m_p={m_p} "
+        )
+    m_1 = quicksort_and_count_med(arr, start=start, stop=i_p - 1)
+    m_2 = quicksort_and_count_med(arr, start=i_p + 1, stop=stop)
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(f"{arr} \t| Quicksort: arr[{start:>2}:{stop+1: <2}] -->  \t| m_p + m_1 + m_2 = {m_p}+{m_1}+{m_2}={m_p + m_1 + m_2}")
+    return m_p + m_1 + m_2
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from ast import literal_eval
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="Number of comparisons in quicksort",
+    )
+    parser.add_argument("--spec", help="", default="(4, 5, 2, 3, 1)")  # should give 7
+    # parser.add_argument("--spec", help="", default="(3, 2, 0, 4, 1, 7, 6, 5)")
+    args = parser.parse_args()
+
+    if Path(args.spec).exists():
+        numbers = Path(args.spec).read_text().splitlines()
+    else:
+        numbers = literal_eval(args.spec)
+
+    LOGGER.info(f"Found {len(numbers)} numbers in array")
+
+    arr = array.array("I", map(int, numbers))
+
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(str(arr) + " <-- original")
+    ans = quicksort_and_count_first(arr, start=0, stop=len(arr) - 1)
+    LOGGER.info(ans)
+    if LOGGER.isEnabledFor(logging.DEBUG):
+        LOGGER.debug(str(arr) + " <-- sorted")
